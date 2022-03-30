62405e7da51936385a68aedcecc52ac50ff5ae59
srirachy <caseyereyes@gmail.com>
2022-03-30 00:26:36 -0700

    add css for eightball section and input section, reorganize create and initialize section, add sound for eightball shake

--- js/eight_ball.js
+++ js/eight_ball.js
@@ -1,52 +1,79 @@
-//initialize body element, call build skeleton, then initialize elements from skeleton
+//initialize body element
 const theBody = document.getElementsByTagName('body')[0];
 //call build skeleton to create html components
 buildSkeleton();
+//initialize elemenets from skeleton
 const theHeader = document.getElementsByTagName('header')[0];
 const theMain = document.getElementsByTagName('main')[0];
 const theFooter = document.getElementsByTagName('footer')[0];
-
-//create ball divider
+//create elements
 const ballDiv = document.createElement('div');
-ballDiv.id = 'answers';
-sectOne.append(ballDiv);
-//ballImg
 const ballImg = document.createElement('img');
+const inputContain = document.createElement('div');
+const qHeader = document.createElement('h2');
+const inputLabel = document.createElement('label');
+const userInput = document.createElement('input');
+const buttonContain = document.createElement('div');
+const askButton = document.createElement('button');
+const yeetButton = document.createElement('button');
+//set ids
+ballDiv.id = 'answers';
 ballImg.id = 'eightball';
+inputContain.id = 'inputDiv';
+qHeader.id = 'qHeader';
+inputLabel.id = 'inputLabel';
+userInput.id = 'theInput';
+buttonContain.id = 'buttonDiv';
+askButton.id = 'askButton';
+yeetButton.id = 'yeetButton';
+//set attributes
+inputLabel.setAttribute('for', 'theInput');
+userInput.setAttribute('type', 'text');
+userInput.setAttribute('placeholder', 'Enter Question');
+//set text content
+inputLabel.textContent = 'Ask Me Anything!';
+askButton.textContent = 'Ask It';
+yeetButton.textContent = 'Yeet It';
+qHeader.textContent = "placeholder";
+//append elements
+sectOne.append(ballDiv);
 ballDiv.append(ballImg);
+sectTwo.append(inputContain);
+inputContain.append(qHeader);
+inputContain.append(inputLabel);
+inputContain.append(userInput);
+sectTwo.append(buttonContain);
+buttonContain.append(askButton);
+buttonContain.append(yeetButton);
+//set to idle eight ball
 ballImg.src = './img/magic8ball_start.png';
 ballImg.alt = 'Idle magic eightball image';
-//user input
-const userInput = document.createElement("input");
-userInput.id = 'theInput'
-userInput.setAttribute("type", "text");
-userInput.setAttribute("placeholder", "Ask me anything!");
-sectOne.append(userInput);
-//ask button
-const askButton = document.createElement("button");
-askButton.id = 'askButton';
-askButton.textContent = 'meow';
-sectOne.append(askButton);
-//yeet button
-const yeetButton = document.createElement("button");
-yeetButton.id = 'yeetButton';
-yeetButton.textContent = 'yeet'
-sectOne.append(yeetButton);
+//initialize buttons by id (mostly for toggle function)
+const askButtonById = document.getElementById('askButton');
+const yeetButtonById = document.getElementById('yeetButton');
 
 //build out the template using template literal 
 function buildSkeleton(){
     const skeleton = `
     <header>
+
     </header>
 
     <main>
-        <section id="sectOne">
-        </section>
+        <article id="theArticle">
+            <section id="sectOne">
+            </section>
+            <section id="sectTwo">
+            </section>
+        </article>
         <aside>
-        <audio id="yeet">
-            <source src="./media/Yeet-sound-effect.mp3" type="audio/mpeg">
-        </audio>
-    </aside>
+            <audio id="shake">
+                <source src="./media/shake.wav" type="audio/wav">
+            </audio>
+            <audio id="yeet">
+                <source src="./media/yeet.mp3" type="audio/mpeg">
+            </audio>
+        </aside>
     </main>
 
     <footer>
@@ -58,50 +85,74 @@ function buildSkeleton(){
 
 //ask button functionality
 askButton.addEventListener('click',() => {
+    toggleButton(askButton, yeetButton);
+    playShakeSound();
     ballImg.classList.add('shakeIt');
-    ballImg.src = './img/magic8ball_start.png';
-    ballImg.alt = 'Magic eightball in motion';
+    resetEightBall(askButton);
+    //animation segment
     setTimeout(() => {
         ballImg.classList.remove('shakeIt');
-        const rNum = getRandomNum();
-        console.log(rNum);
-        const rImg = getImgName(rNum);
-        const rAccText = getAccText(rNum);
-        ballImg.src = rImg;
-        ballImg.alt = rAccText;
+        setEightBall();
+        toggleButton(askButton, yeetButton);
     }, 1000)
 });
 
 //yeet button functionality
 yeetButton.addEventListener('click', () => {
+    toggleButton(yeetButton, askButton);
     yellYeet();
     ballImg.classList.add('yeetIt');
-    ballImg.src = './img/magic8ball_start.png';
-    ballImg.alt = 'Magic eightball thrown';
-    yeetButton.disabled = true;
+    resetEightBall();
+    //animation segment
     setTimeout(() => {
         ballImg.classList.remove('yeetIt');
         ballImg.classList.add('rollIn');
         setTimeout(() => {
             ballImg.classList.remove('rollIn');
-            const rNum = getRandomNum();
-            console.log(rNum);
-            const rImg = getImgName(rNum);
-            const rAccText = getAccText(rNum);
-            ballImg.src = rImg;
-            ballImg.alt = rAccText;
+            setEightBall();
+            toggleButton(yeetButton, askButton);
         }, 3000)
     }, 500)
-    yeetButton.disabled = false;
-}
-);
+});
+
+// reset eight ball
+const resetEightBall = (theButton) => {
+    ballImg.src = './img/magic8ball_start.png';
+    if (theButton == askButton){
+        ballImg.alt = 'Magic eightball is shaking';
+    } else {
+        ballImg.alt = 'Magic eightball thrown';
+    }
+};
+
+// set eight ball to random image
+const setEightBall = () => {
+    const rNum = getRandomNum();
+    const rImg = getImgName(rNum);
+    const rAccText = getAccText(rNum);
+    ballImg.src = rImg;
+    ballImg.alt = rAccText;
+};
+
+// toggle enable/disable button
+const toggleButton = (curButton, otherButton) => {
+    //toggle current button
+    (curButton.disabled === true) ? curButton.disabled = false : curButton.disabled = true;
+    //toggle other button
+    (otherButton.disabled === true) ? otherButton.disabled = false : otherButton.disabled = true;
+};
+
+// play eight ball shake sound
+const playShakeSound = () => {
+    theSound = document.getElementById('shake');
+    theSound.play();
+};
 
 //  play yeet sound
 const yellYeet = () => {
     let yeet = document.getElementById("yeet");
     yeet.play();
-    return;
-}
+};
 
 //tenary to create string name of image ft math.random to generate random number between 1-20
 const getRandomNum = () => {
@@ -159,6 +210,5 @@ const getAccText = (randoNum) => {
             return 'Eightball says: Very doubtful';
         default:
             return 'This should never happen';
-
     }
 };
